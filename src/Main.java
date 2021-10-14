3934823dcbb5709f7f6bf68e228764878bbd5ed6
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2021-10-14 20:33:24 +0300

    began works with Controller

--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,98 @@
+import vehicle.Vehicle;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private final Scanner _scanner;
     private final Menu _view;
     private final User _user;
-    private String _enteredPassword = "";
+    private String _enteredPassword;
+    private List<Vehicle> vehicles;
 
-    Main(Menu view, User user) throws InterruptedException, IOException {
+    Main(Menu view, User user) throws InterruptedException {
+        this._scanner = new Scanner(System.in);
         this._user = user;
         this._view = view;
         _view.welcome(_user.getName());
-        _view.userData(_user);
         tryEnterPassword();
+        _view.userData(_user);
     }
 
     private void tryEnterPassword() throws InterruptedException {
         int attempts = 3;
-        Scanner sc = new Scanner(System.in);
         while (attempts != 0) {
-            String password = sc.nextLine();
+            String password = _scanner.nextLine();
             if (_user.checkPassword(password)) {
                 this._enteredPassword =  password;
+                _view.validPassword();
                 break;
-            }
-            attempts--;
+            } attempts--;
             _view.invalidPassword(attempts);
         }
-        sc.close();
-        if (attempts <= 0) {
+        if (attempts <= 0)
             throw new InterruptedException("-1");
-        }
-
     }
 
     private int enterCommand() {
-        Scanner sc = new Scanner(System.in);
-        int result = 0;
-        while(true) {
-            if (sc.hasNext()) {
-                result = sc.nextInt();
+        int result = -1;
+        while (true) {
+            String buffer = _scanner.nextLine();
+            try {
+                result = Integer.parseInt(buffer);
                 break;
-            } else {
-                _view.errorCommand(result);
-                sc.nextLine();
+            } catch (NumberFormatException e) {
+                _view.errorCommand(buffer);
             }
-        } sc.close();
-        return result;
+        } return result;
     }
 
-    private void chooseCommand(int command) throws InterruptedException { // TODO
+    private void chooseCommand(int command) throws InterruptedException {
         switch (command) {
             case (0):
                 throw new InterruptedException("0");
 
             case (1):
-                //  TODO - operation 1
+                loadVehicle();
+                _view.workWitList(vehicles);
+                runBD();
                 break;
 
             case (2):
-                //  TODO - operation 2
+                vehicles = new ArrayList<>();
+                _view.workWitList(vehicles);
+                runBD();
                 break;
+
             default:
                 if (_user.isSudoMode())
                     chooseSudoCommand(command);
                 else
-                    _view.errorCommand(command);
+                    _view.errorCommand(String.valueOf(command));
         }
     }
+
+    private void loadVehicle() {}
+    private void runBD() {}
+    private void runAutoTests() {}
+
 
     private void chooseSudoCommand(int command){
         switch (command)  {
             case (3):
-                //  TODO - launch auto tests
-                break;
-
-            case (4):
-                //  TODO -
+                runAutoTests();
                 break;
 
             default:
-                _view.errorCommand(command);
+                _view.errorCommand(String.valueOf(command));
         }
     }
 
     public void run() throws InterruptedException {
-        int command = enterCommand();
         _view.show(_user.isSudoMode());
+        int command = enterCommand();
         chooseCommand(command);
-    }
-
-    public String getEnteredPassword() {
-        return _enteredPassword;
     }
 }
