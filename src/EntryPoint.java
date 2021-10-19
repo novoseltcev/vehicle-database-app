c6c4f321564906e19cadd92dbbd88357b1f47262
Stanislav Novoseltcev <73393494+st-a-novoseltcev@users.noreply.github.com>
2021-10-19 17:37:36 +0300

    Add files via upload

--- src/EntryPoint.java
+++ src/EntryPoint.java
@@ -1,3 +1,5 @@
+import java.util.logging.Level;
+
 import ctr.MainCtrl;
 import model.User;
 import view.CrashNotifier;
@@ -10,7 +12,16 @@ public class EntryPoint {
             MainMenu view = new MainMenu(user.getLangData());
             MainCtrl controller = new MainCtrl(view, user);
             while (true) {
-                controller.run();
+            	try {
+            		controller.run();
+            	} catch (InterruptedException e) {
+            		MainCtrl.logger.log(Level.INFO, "Close programm with code: " + e.getMessage());
+            		throw e;
+            	} catch (Exception e) {
+            		MainCtrl.logger.log(Level.SEVERE, "Interrupt programm by cause:  " + e.getMessage());
+            		throw e;
+            	}
+            	
             }
         } catch (Exception e) {
             CrashNotifier crashNotifier = new CrashNotifier();
