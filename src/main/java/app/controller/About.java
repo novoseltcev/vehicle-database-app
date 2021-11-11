cd9a4243a0386b5e1c1b029f301aa1faa300d758
st-a-novoseltcev <st.a.novoseltcev@gmail.com>
2021-11-12 02:34:37 +0300

    add about me github-card

---
+++ src/main/java/app/controller/About.java
@@ -0,0 +1,96 @@
+package app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+import java.util.*;
+
+
+public class About {
+    @FXML
+    private ImageView avatarImg;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label loginLabel;
+
+
+    @FXML
+    private Label reposLabel;
+
+    @FXML
+    private Label gistsLabel;
+
+    @FXML
+    private Label followersLabel;
+
+    @FXML
+    private Label bioLabel;
+
+    @FXML
+    private Label companyLabel;
+
+    @FXML
+    private Label locationLabel;
+
+
+    @FXML
+    private void initialize() throws InterruptedException {
+        try {
+            URL githubApi = new URL("https://api.github.com/users/st-a-novoseltcev");
+            Scanner scanner = new Scanner(githubApi.openStream());
+            String response = scanner.useDelimiter("\\Z").next();
+            scanner.close();
+
+            for (String i: response.split("\","))
+                System.out.println(i);
+
+            Map<String, String> json = parseJSON(response);
+            System.out.println(json);
+
+            URL avatarURL = new URL(json.get("avatar_url"));
+            Image avatar = new Image(avatarURL.toString());
+            avatarImg.setSmooth(true);
+            avatarImg.setImage(avatar);
+
+            loginLabel.setText(json.get("login"));
+            nameLabel.setText(json.get("name"));
+
+            gistsLabel.setText(json.get("public_gists"));
+            reposLabel.setText(json.get("public_repos"));
+            followersLabel.setText(json.get("followers"));
+
+            bioLabel.setText(json.get("bio"));
+            companyLabel.setText(json.get("company"));
+            locationLabel.setText(json.get("location"));
+
+        } catch (IOException e) {
+            Thread.sleep(3 * 1000);
+            initialize();
+        }
+    }
+
+    private Map<String, String> parseJSON(String json) {
+        HashMap<String, String> result = new HashMap<>();
+        String normalizeJson = json.replaceAll("\\{\"", "").replaceAll("\"}", "");
+        String[] newJson = normalizeJson.split(",\"");
+        for (String str: newJson) {
+            String[] split = str.split("\":");
+            result.put(split[0], split[1].replaceAll("\"", ""));
+        }
+        return result;
+    }
+}
